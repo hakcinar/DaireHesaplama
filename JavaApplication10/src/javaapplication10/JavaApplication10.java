ae37ead8122e25978355b279f1c05076184da0ef
hakcinar <44376537+hakcinar@users.noreply.github.com>
2021-06-25 12:45:45 +0300

    Add files via upload

---
+++ JavaApplication10/src/javaapplication10/JavaApplication10.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication10;
+
+/**
+ *
+ * @author Akcinar Ailesi
+ */
+public class JavaApplication10 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+int points=6;
+switch(points)
+{
+  case 6: ;
+  case 7: System.out.println("Java");break;
+  case 8: ;
+  case 9: System.out.println("101");break;
+  case 10: System.out.println("Patika"); break;
+  default: System.out.println("Dev");
+}
+    }}
